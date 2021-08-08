d71dcb569134f67e62025f845f94cd1c826352d5
Dhani <sh.ramadhani17@gmail.com>
2021-08-09 01:43:35 +0700

    first commit

--- contact.php
+++ contact.php
@@ -8,11 +8,12 @@ $message= $_POST['message'];
 $to = "sh.ramadhani17@gmail.com";
 $subject = "Retro Light";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n\ phone=".$phone. "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+
+$headers = "From: harryramadhani17@gmail.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+header("Location:retroligt.html");
 ?>
